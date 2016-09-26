5a19edd292a3a29a4f8bcd4d5fd88f36dd60fb4b
Jan Filip Kotora <traktorista.pilot@gmail.com>
2016-09-26 22:50:55 +0200

    Opravil som chybu v preklade

--- hanoi.c
+++ hanoi.c
@@ -7,7 +7,7 @@ void hanoi(int n, int a, int b)
   else
   {
     hanoi(n - 1, a, 3 - a - b);
-    printf("presin kamen %d z %d do %d\n", n, a, b);
+    printf("presun kamen %d z %d do %d\n", n, a, b);
     hanoi(n - 1, 3 - a - b, b);
   }
 }
